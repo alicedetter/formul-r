bc59e49a587d518483818914a0eb2ea6b6aedbc3
alicedetter <detteralice@gmail.com>
2025-12-05 10:41:41 +0100

    h

--- index.php
+++ index.php
@@ -1,4 +1,14 @@
 <!DOCTYPE html>
+<?php
+$textmassa ="";
+
+if(isset($_POST['btn'])){
+    $from_hidden=$_POST['hide_me'];
+    $from_text=$_POST['skriv'];
+    $textmassa=$from_hidden." ".$from_text;
+}
+?>
+
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -6,6 +16,13 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <form action="index.php" method="POST">
+        <textarea name="skriv" id="skriv"></textarea>
+        <input type="hidden" name="hide_me" value="<?=$textmassa?>">
+        <input type="submit" name="btn" value="Skicka in">
+    </form>
+    <div>
+        <h1><?=$textmassa?></h1>
+    </div>
 </body>
 </html>
